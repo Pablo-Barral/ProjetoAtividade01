079b284aa31ac908e910808b9ef31b21dd3cc51b
Pablo-Barral <95885735+Pablo-Barral@users.noreply.github.com>
2022-02-04 11:16:34 -0300

    Fix: caminho para os icones de climas

--- modules/weatherIcon.js
+++ modules/weatherIcon.js
@@ -1,21 +1,21 @@
 export default function weatherIcon(icon, id) {
   if (id.innerHTML.includes("thunder")) {
-    icon.innerHTML = "../assets/thunder.png";
+    icon.innerHTML = ".././assets/thunder.png";
   }
   if (id.innerHTML.includes("rain")) {
-    icon.innerHTML = "../assets/raining.png";
+    icon.innerHTML = ".././assets/raining.png";
   }
   if (id.innerHTML.includes("Fog") || id.innerHTML.includes("Mist")) {
-    icon.innerHTML = "../assets/cloud.png";
+    icon.innerHTML = ".././assets/cloud.png";
   }
   if (id.innerHTML.includes("Cloudy") || id.innerHTML.includes("Overcast")) {
-    icon.innerHTML = "../assets/cloudy.png";
+    icon.innerHTML = ".././assets/cloudy.png";
   }
   if (id.innerHTML.includes("Sunny")) {
-    icon.innerHTML = "../assets/sun.png";
+    icon.innerHTML = ".././assets/sun.png";
   }
   if (id.innerHTML.includes("thunder") && id.includes("rain")) {
-    icon.innerHTML = "../assets/storm.png";
+    icon.innerHTML = ".././assets/storm.png";
   }
   return icon;
 }
