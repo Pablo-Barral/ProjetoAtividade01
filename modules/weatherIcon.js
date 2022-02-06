be4bd7576e0ffd081cf622bbc3dd6326615b8188
Pablo Barral <pablo.barral.kts@gmail.com>
2022-02-05 22:15:00 -0300

    Fix: otimização lógica do clima

--- modules/weatherIcon.js
+++ modules/weatherIcon.js
@@ -1,20 +1,18 @@
 export default function weatherIcon(icon, id) {
   if (id.innerHTML.includes("thunder")) {
     icon.innerHTML = "./assets/thunder.png";
-  }
-  if (id.innerHTML.includes("rain")) {
+  } else if (id.innerHTML.includes("rain")) {
     icon.innerHTML = "./assets/raining.png";
-  }
-  if (id.innerHTML.includes("Fog") || id.innerHTML.includes("Mist")) {
+  } else if (id.innerHTML.includes("Fog") || id.innerHTML.includes("Mist")) {
     icon.innerHTML = "./assets/cloud.png";
-  }
-  if (id.innerHTML.includes("Cloudy") || id.innerHTML.includes("Overcast")) {
+  } else if (
+    id.innerHTML.includes("Cloudy") ||
+    id.innerHTML.includes("Overcast")
+  ) {
     icon.innerHTML = "./assets/cloudy.png";
-  }
-  if (id.innerHTML.includes("Sunny")) {
+  } else if (id.innerHTML.includes("Sunny")) {
     icon.innerHTML = "./assets/sun.png";
-  }
-  if (id.innerHTML.includes("thunder") && id.includes("rain")) {
+  } else if (id.innerHTML.includes("thunder") && id.includes("rain")) {
     icon.innerHTML = "./assets/storm.png";
   }
   return icon;
